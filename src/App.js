0dea86cc2c3029986954edde69cdc292011bcee7
Sveta <sveta.pavl@tut.by>
2021-05-19 21:49:07 +0300

    React for Github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import TodoList from './Todo/TodoList'
+import Context from './context'
+import AddTodo from './Todo/AddTodo'
 
 function App() {
+  const [todos, setTodos] = React.useState([
+    {id: 1, completed: false, title: 'Сходить в магазин за продуктами'},
+    {id: 2, completed: true, title: 'Убрать в квартире'},
+    {id: 3, completed: false, title: 'Сделать приложение на React'},
+    {id: 4, completed: false, title: 'Сделать приложение на Redux'}
+  ])
+  
+  function toggleTodo(id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+    )
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  function addTodo(title) {
+    setTodos(todos.concat([
+        {
+          title,
+          id: Date.now(),
+          completed: false
+        }
+      ])
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Context.Provider value={{ removeTodo }}>
+      <div className="wrapper">
+        <h1>Список задач</h1>
+        <AddTodo onCreate={addTodo} />
+        {todos.length ? (<TodoList todos={todos} onToggle={toggleTodo} />) : (<p>Задач пока нет!</p>)}
+        
+      </div>
+    </Context.Provider>
+  )
 }
 
 export default App;
